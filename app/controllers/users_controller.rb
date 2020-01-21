27001943ba548e59167f828c38cd8c78daaeee27
github username <github email address>
2020-01-21 17:25:47 -0500

    no

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,15 @@ class UsersController < ApplicationController
 
     private
 
-<<<<<<< HEAD
-=======
     def serialized_data  
-        {
-            :except => [:created_at, :updated_at],
-            :include =>  [
+        {:except => [:created_at, :updated_at],
+          :include =>  [
               :characters => {:except => [:created_at, :updated_at]},
               :spells     => {:except => [:created_at, :updated_at]}
-            ]
-        }
+        ]
+    }
     end
 
->>>>>>> 1d68bcefb99f45c571abc738677b451e5a1ba001
     def user_params
         params.require(:user).permit(:name, :house)
     end
